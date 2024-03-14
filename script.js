439ddf0bbd600bcb01102dc1a03d5bd135f9a233
Thuong1310 <146789705+Thuong1310@users.noreply.github.com>
2024-03-15 01:36:44 +0700

    add_fileweb_laptopcongthuong

---
+++ script.js
@@ -0,0 +1,7 @@
+
+document.getElementById('register-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+    var username = document.getElementById('register-username').value;
+    var email = document.getElementById('register-email').value;
+    var password = document.getElementById('register-password').value;
+});
